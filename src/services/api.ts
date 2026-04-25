1bb8ef4fbaa12a0bc23bccf1ba60da0fbc5c7b7c
Finney Solomon <finney.solomon.1@gmail.com>
2026-04-25 18:15:33 +0530

    fix: Update API base URL to production endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import { Blog } from "./types/blogsTypes";
 
-const API_BASE_URL = "http://localhost:5858/api";
+// const API_BASE_URL = "http://localhost:5858/api";
+const API_BASE_URL = "https://futurefoods-api.vercel.app";
 
 export type ApiUser = {
   id: string;
